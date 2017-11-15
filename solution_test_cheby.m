bcaa92144566785c48611c21772858f86c50e6ae
Juliana Chen <jctwinkle@gmail.com>
2017-11-14 19:19:44 -0800

    work on loop with cheby approx in 3D (with 2 discrete vars in another dimension) with comments from Italo 11/14

--- solution_test_cheby.m
+++ solution_test_cheby.m
@@ -25,19 +25,13 @@ for t = G.n_period-1:-1:1
     end
     
     % Chevyshev Approximation
-    B = kron(T_A, kron(T_H,T_K)); % half of the numerator of the coeff
-    Num = TVF'*B; % TVF'*kron(T_A, kron(T_H,T_K)); % numerator (bases*function) 
+    Num = Emax'*kron(T_A, kron(T_H,T_K)); % numerator (bases*function) 
     Den = kron(T2_A, kron(T2_H,T2_K)); % square of T multiplied
     for x = 1:1:(n_matstat*n_wrkexp)
         alpha(x,:) = Num(x,:)./Den';
     end
-    % alpha contains 20 rows of 19x4x4 = 304 coefficients
+    % alpha contains 20 rows of 19x4x4 = 304 coefficients  
     
-    % maybe won't need this
-    assets_wide = linspace(-5,100,n_assets); % probably going to change by period
-    childHC_wide = linspace(-5,10,n_childK);
-    workexp_wide = linspace(-1,10,n_wrkexp);    
-
     % loop for shocks (27):
     for i = 1:1:G.n_shocks % 27 x 3
         i
@@ -49,7 +43,7 @@ for t = G.n_period-1:-1:1
         % loop for work experience and marital status (20):
         for x = 1:1:(n_matstat*n_wrkexp)
             x;
-            coeffs = alpha(1,:);
+            
             % current state variables:
             m_j = SS_M(x);  % marital status
             n_j = SS_N(x);  % children
@@ -69,158 +63,131 @@ for t = G.n_period-1:-1:1
             w_j_n = exp(alpha1*X_j + alpha2*edu + abi + shock_n); % same
             w_j_u = 0; % unemployed women don't have earnings?
               
-            % probabilities:      
-            lambda_r = (edu + X_j + abi)/10; % currently has abi, shouldn't
-            pi_r = 0.2; % one Emax for temporary/unemployed and one Emax for all
-            
             % sector-specific probabilities: % CHECK THIS WHEN AGE VARIES
             prob_marr_r = (edu + abi + 35 + t)/100; % only change with time & type
             prob_marr_n = (edu + abi + 30 + t)/100; % only change with time & type
             prob_marr_u = (edu + abi + 25 + t)/100; % only change with time & type
-            probably = a0 + a1*col4 + a2*col2 + a3*work + a4*t;
+            %probability = a0 + a1*col4 + a2*col2 + a3*work + a4*t;
             
             % transitions for exogenous variables:
-            wh_next = wh_j;
             K_next = K_j + inv; % make a function (CES)
+            wh_next = wh_j; % no transition
             
-            % save wages in R and N for all states and all shocks
-            % wages_r_allstates = 216 x 23
+            % consumption vector
+            c_vector = unique(assets); % consumption vector (unique assets, 20)
+            %%%need to change this, so that if A_j=1, cons go from 0 to 1
+            %%%you also have income and lowest shock, 0 to max chh based on
+            %%%A_next function
             
-            % make consumption vector HERE (for each time-shock-state)
-            % from 0 to max potential assets
-            %c_vector = ;
-    %%%%%%%%% this is how you adjust the linepsace
-%     ubT= inc(1,3) + (1+P.r)*a_(:,3);
-%     for j=1:G.nss
-%       c(j,:)= linspace(0,ubT(j),G.Nc);
-%       minv(j,:)= linspace(0,ubT(j),G.Nc);
-%     end
-%     tinv= [0,0.5,1];
-
-            % loop over consumption (5):
+            % loop over consumption
             for k = 1:1:length(c_vector)
                 k;
                 
                 chh = c_vector(k); % HH consumption
-                cw = (1+delta1*m_j+delta2*n_j)*chh; % woman's consumption - can this be larger than HH consumption?
+                cw = 0.5*chh; % woman's consumption
                 
-                % single:
+                % single & married, get all six :
                 if x <= 10
-                    
+
                     % regular job:
-                    X_next = X_j + 1;
                     A_next = (1+r) * (A_j + (w_j_r + wh_j*m_j + shock_i) - chh - n_j*inv); % eq. 8
-                    if prob_marr_r > 0.5
-                        m_next = m_j + 1;
-                        n_next = n_j + 1;
-                    else
-                        m_next = m_j;
-                        n_next = n_j;
+                    x_next = x + 1;
+                    if x_next == 11
+                        x_next = 10;
                     end
-                    if m_next > 1 % limit to less than 1
-                        m_next = 1;
-                        n_next = 1;
-                    end
-                    
+                    %%%% validate A_next compared to extmin_A and extmax_A
                     % value function:
-                    V_r_next = interpn(childHC_wide,assets_wide,hearnings,workexp_wide,matstat, tmp, K_next,A_next,wh_next,X_next,m_next);
+                    Base=kron(chebpoly_base(18+1, d_A*(A_next - extmin_A) - 1),kron(chebpoly_base(3+1, d_H*(wh_next - extmin_H) - 1),chebpoly_base(3+1, d_K*(K_next - extmin_K) - 1)));
+                    Vs_r_next = sum(alpha(x_next,:).*Base,2); %cheby_approx(coeffs,ncheby,extminA,extminB,extminC,dA,dB,dC,A_next,B_next,C_next)
 
                     % non-regular job:
-                    X_next = X_j + 1;
                     A_next = (1+r) * (A_j + (w_j_n + wh_j*m_j + shock_i) - chh - n_j*inv);
-                    if prob_marr_n > 0.5
-                        m_next = m_j + 1;
-                        n_next = n_j + 1;
-                    else
-                        m_next = m_j;
-                        n_next = n_j;
+                    x_next = x + 1;
+                    if x_next == 11
+                        x_next = 10;
                     end
-                    if m_next > 1 % limit to less than 1
-                        m_next = 1;
-                        n_next = 1;
-                    end
-                    
                     % value function:
-                    V_n_next = interpn(childHC_wide,assets_wide,hearnings,workexp_wide,matstat, tmp, K_next,A_next,wh_next,X_next,m_next);
+                    Base=kron(chebpoly_base(18+1, d_A*(A_next - extmin_A) - 1),kron(chebpoly_base(3+1, d_H*(wh_next - extmin_H) - 1),chebpoly_base(3+1, d_K*(K_next - extmin_K) - 1)));
+                    Vs_n_next = sum(alpha(x_next,:).*Base,2); 
                     
                     % unemployed:
-                    X_next = X_j + 0;
                     A_next = (1+r) * (A_j + (w_j_u + wh_j*m_j + shock_i) - chh - n_j*inv);
-                    if prob_marr_u > 0.5
-                        m_next = m_j + 1;
-                        n_next = n_j + 1;
-                    else
-                        m_next = m_j;
-                        n_next = n_j;
-                    end
-                    if m_next > 1 % limit to less than 1
-                        m_next = 1;
-                        n_next = 1;
-                    end
-                    
+                    x_next = x;
                     % value function:
-                    V_u_next = interpn(childHC_wide,assets_wide,hearnings,workexp_wide,matstat, tmp, K_next,A_next,wh_next,X_next,m_next);
-                    
-                else   
+                    Base=kron(chebpoly_base(18+1, d_A*(A_next - extmin_A) - 1),kron(chebpoly_base(3+1, d_H*(wh_next - extmin_H) - 1),chebpoly_base(3+1, d_K*(K_next - extmin_K) - 1)));
+                    Vs_u_next = sum(alpha(x_next,:).*Base,2); % extmin_A was NOT low enough (0.99, A_next = -0.4144), can we change each approx?
+                       
                 % Married:
-                
+                else
                     % Regular:
-                    X_next = X_j + 1;
                     A_next = (1+r) * (A_j + (w_j_r + wh_j*m_j + shock_i) - chh - n_j*inv);
-                    m_next = m_j;
-                    n_next = n_j;
-                    Vm_r_next = ;
+                    x_next = x + 1;
+                    if x_next == 21
+                        x_next = 20;
+                    end
+                    Base=kron(chebpoly_base(18+1, d_A*(A_next - extmin_A) - 1),kron(chebpoly_base(3+1, d_H*(wh_next - extmin_H) - 1),chebpoly_base(3+1, d_K*(K_next - extmin_K) - 1)));
+                    Vm_r_next = sum(alpha(x_next,:).*Base,2);
                     
                     % Non-regular:
-                    X_next = X_j + 1;
                     A_next = (1+r) * (A_j + (w_j_n + wh_j*m_j + shock_i) - chh - n_j*inv);
-                    m_next = m_j;
-                    n_next = n_j;
-                    Vm_n_next = ;
+                    x_next = x + 1;
+                    if x_next == 21
+                        x_next = 20;
+                    end
+                    Base=kron(chebpoly_base(18+1, d_A*(A_next - extmin_A) - 1),kron(chebpoly_base(3+1, d_H*(wh_next - extmin_H) - 1),chebpoly_base(3+1, d_K*(K_next - extmin_K) - 1)));
+                    Vm_n_next = sum(alpha(x_next,:).*Base,2);
                     
                     % Unemployed:
-                    X_next = X_j + 0;
                     A_next = (1+r) * (A_j + (w_j_u + wh_j*m_j + shock_i) - chh - n_j*inv);
-                    m_next = m_j;
-                    n_next = n_j;
-                    Vm_u_next = ;
-                    
+                    x_next = x;
+                    Base=kron(chebpoly_base(18+1, d_A*(A_next - extmin_A) - 1),kron(chebpoly_base(3+1, d_H*(wh_next - extmin_H) - 1),chebpoly_base(3+1, d_K*(K_next - extmin_K) - 1)));
+                    Vm_u_next = sum(alpha(x_next,:).*Base,2);
+                    %%%%save A_next and consumption
                 end
                 
-                % Together:
-                    
-                    % Sector-Specific Utility:
-                    u_r(k) = (cw^(1-sigma))/(1-sigma) + psi_r + kappa*(1+theta1*m_j+theta2*n_j+theta3*K_j);
-                    u_n(k) = (cw^(1-sigma))/(1-sigma) + psi_n + kappa*(1+theta1*m_j+theta2*n_j+theta3*K_j);
-                    u_u(k) = (cw^(1-sigma))/(1-sigma) + kappa*(1+theta1*m_j+theta2*n_j+theta3*K_j);
-                    
-                    % Expected Utility:
-                    EU = max([V_r_next,V_n_next,V_u_next,Vm_r_next,Vm_n_next, Vm_u_next]);
-                    
-                    % Sector-Specific Value Functions:
-                    V_r(k) = u_r(k) + beta * EU;
-                    V_n(k) = u_n(k) + beta * EU;
-                    V_u(k) = u_u(k) + beta * EU;         
+                % Sector-Specific Utility:
+                u_r(k) = (cw^(1-sigma))/(1-sigma) + psi_r + kappa*(1+theta1*m_j+theta2*n_j+theta3*K_j);
+                u_n(k) = (cw^(1-sigma))/(1-sigma) + psi_n + kappa*(1+theta1*m_j+theta2*n_j+theta3*K_j);
+                u_u(k) = (cw^(1-sigma))/(1-sigma) + kappa*(1+theta1*m_j+theta2*n_j+theta3*K_j);
+                
+                if x <= 10
+                    % Sector-Specific Value Functions
+                    Vs_r(k) = u_r(k) + beta * ((1-prob_marr_r)*Vs_r_next + prob_marr_r*Vm_r_next);
+                    Vs_n(k) = u_n(k) + beta * ((1-prob_marr_n)*Vs_n_next + prob_marr_n*Vm_n_next);
+                    Vs_u(k) = u_u(k) + beta * ((1-prob_marr_u)*Vs_u_next + prob_marr_u*Vm_u_next); 
+                else
+                    % Sector-Specific Value Functions
+                    Vm_r(k) = u_r(k) + beta * Vm_r_next;
+                    Vm_n(k) = u_n(k) + beta * Vm_n_next;
+                    Vm_u(k) = u_u(k) + beta * Vm_u_next;
+                end
             end
             
+            %%% validate that consumption is feasible
+            %%% check that A_next is within extmin_A and extmin_B
                 % save optimal V* & c*
-                %%%validate that consumption is feasible
-                [V_r_star, Index_r_k] = max(V_r);
-                [V_n_star, Index_n_k] = max(V_n);
-                [V_u_star, Index_u_k] = max(V_u);
-                c_r_star = c_vector(Index_r_k);
-                c_n_star = c_vector(Index_n_k);
-                c_u_star = c_vector(Index_u_k);
+                [Vs_r_star, Index_sr_k] = max(Vs_r);
+                [Vs_n_star, Index_sn_k] = max(Vs_n);
+                [Vs_u_star, Index_su_k] = max(Vs_u);
+                [Vm_r_star, Index_mr_k] = max(Vm_r);
+                [Vm_n_star, Index_mn_k] = max(Vm_n);
+                [Vm_u_star, Index_mu_k] = max(Vm_u);
+                cs_r_star = c_vector(Index_sr_k);
+                cs_n_star = c_vector(Index_sn_k);
+                cs_u_star = c_vector(Index_su_k);
+                cm_r_star = c_vector(Index_mr_k);
+                cm_n_star = c_vector(Index_mn_k);
+                cm_u_star = c_vector(Index_mu_k);
                 c_star_vector = [c_r_star, c_n_star, c_u_star];
                 
                 % save labor choice:
                 [V_star, Index_l] = max([V_r_star,V_n_star,V_u_star]);
-                c_star(j, i, t) = c_star_vector(Index_l);
+                c_star(j, i, t) = c_star_vector(Index_l); %c_star(j,i,s,t)
                 l_star(j, i, t) = Index_l;
                 V_star_aux(j, i, t) = V_star; 
                 
         end
-        
+        end
     end
     
     % Integrate over shocks
@@ -228,3 +195,6 @@ for t = G.n_period-1:-1:1
 %     Em(:,t,k) = detR*detV^(-1/2)*pi^(-(size(R,1))/2)*G.w(i)*G.w(j)*V + Em(:,t,k);
     W = pi^(-1/2)*V_star_aux(:,:,t)*weight;   
 end
+%%% time this shit
+% revers order of matstat, to do 10 rows of married first and then 10 rows
+% of single, and then when single i can compare 6 value functions
